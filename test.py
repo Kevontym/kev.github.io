c73108db28f11dcecdac1f163444d4f1de79ad4f
Kevon Mitchell <99846526+Kevontym@users.noreply.github.com>
2024-01-02 23:58:18 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,38 @@
+# - / // ** * %
+
+print(10 + 3)
+print(10 - 3)
+print(10 / 3)
+print(10 * 3)
+print(10 + 3)
+print(10 % 3)
+print(10 ** 3)
+
+print('Now')
+a = 10
+b = 3
+
+print(10 + 3)
+print(10 - 3)
+print(10 / 3)
+print(10 * 3)
+print(10 + 3)
+print(10 % 3)
+print(10 ** 3)
+
+print('Now calculate and assign...')
+c = 10 + 3
+print(c)
+
+a = 10
+
+a = a +2
+
+b = 20
+b += 1 # same thing as b = b +1
+
+b -= 1 #
+
+import math 
+
+root_two = math.sqrt(2)
